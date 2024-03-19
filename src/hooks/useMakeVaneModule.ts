2ef2e7bcafab7f0e30cee65244bff6795f0efdd5
HANGYESUK <akakakak66@naver.com>
2024-03-19 20:22:32 +0900

    Feature: Create useMakeVaneModule hook - vaneAtom(날개 원자)를 받아 vaneModule(날개)의 data를return하는 hook

---
+++ src/hooks/useMakeVaneModule.ts
@@ -0,0 +1,51 @@
+import {ReactNode, useMemo} from "react";
+
+type useMakeVaneModuleType = {
+    count: number;
+    vaneAtom?: ReactNode;
+    bg?: ReactNode;
+}
+const useMakeVaneModule = ({count, vaneAtom = '🌟', bg = '🌌'}: useMakeVaneModuleType ) => {
+
+    const addVaneAtom = (num:number, xLineArr: ReactNode[]) => {
+        for(let i=0; i<num; i++) {
+            xLineArr.push(vaneAtom)
+        }
+    }
+
+    const addBg = (num:number, xLineArr: ReactNode[]) => {
+        for(let i=0; i<num; i++) {
+            xLineArr.push(bg)
+        }
+
+    }
+
+    const vaneModule = useMemo(() => {
+        const module: ReactNode[] = []
+
+        // y값 만큼 반복
+        for(let i=count; i>0; i--) {
+            const xLineArr: ReactNode[] = []
+            const emptyNum: number = count;
+
+            if(i < emptyNum) {
+                addBg(emptyNum - i, xLineArr)
+            }
+
+            addVaneAtom(i, xLineArr)
+
+            module.push(xLineArr)
+        }
+        
+        return module
+
+
+    }, [count, vaneAtom, bg])
+
+
+    return {
+        vaneModule : [...vaneModule]
+    }
+}
+
+export default useMakeVaneModule;
